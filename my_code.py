2b60709de1f3e1c55857fed97e017a4ec8250e51
Milan938 <23mboga@athenian.org>
2020-09-11 20:57:54 +0000

    Finished

--- my_code.py
+++ my_code.py
@@ -1,4 +1,16 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
-x =input("person")
-print("There was a " + x + " who lived in a lake")
+x =input("name of a man ")
+y =input("verb for going somewhere ")
+z =input("adjective ")
+a =input("adjective ")
+b =input("verb ")
+c =input("adjective ")
+d =input("adjective ")
+e =input("adjective ")
+f =input("verb for going somewhere ")
+print("There was a man named " + x + " who lived on a mountain in the middle of nowhere. Everyday," ,
+"he would go " + y + " to the lake on his mountain. The lake was very " + z + " and " + a + " , and " , 
++ x + " loved to go " + b + " in the lake. One day, a " + c + " and " + d + " animal appeared in the lake" , 
+" and " + x + " wanted to go play with it. He went over to go play with it, but the animal was very " ,
++ e + " , so " + x + " got very scared and " + f + " away. ") 
