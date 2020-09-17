5606692b488901904feb29b07b8de5ee7afdd683
Milan938 <23mboga@athenian.org>
2020-09-17 16:12:59 +0000

    Fixed all errors

--- my_code.py
+++ my_code.py
@@ -1,5 +1,5 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+#  None
 x = input("name of a man ")
 y = input("verb for going somewhere ")
 z = input("adjective ")
@@ -10,8 +10,8 @@ d = input("adjective ")
 e = input("adjective ")
 f = input("verb for going somewhere ")
 
-print("There was a man named " + x + " who lived on a mountain in the middle of nowhere. Everyday," ,
-"he would go " + y + " to the lake on his mountain. The lake was very " + z + " and " + a + " , and " , 
-+ x + " loved to go " + b + " in the lake. One day, a " + c + " and " + d + " animal appeared in the lake" , 
-" and " + x + " wanted to go play with it. He went over to go play with it, but the animal was very " ,
-+ e + " , so " + x + " got very scared and started" + f + " away. ") 
+print("There was a man named ", x, " who lived on a mountain in the middle of nowhere. Everyday," ,
+"he would go ", y, " to the lake on his mountain. The lake was very ", z, " and " , a, " , and " , 
+ x, " loved to go ", b, " in the lake. One day, a " , c, " and " , d, " animal appeared in the lake" , 
+" and ", x, " wanted to go play with it. He went over to go play with it, but the animal was very " ,
+ e, " , so ", x, " got very scared and started", f, " away. ") 
